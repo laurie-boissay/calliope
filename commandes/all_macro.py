f871777aac58f80494f92ddb1c32a120c9943dca
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-05-29 13:13:02 +0200

    Add files via upload

--- commandes/all_macro.py
+++ commandes/all_macro.py
@@ -16,37 +16,49 @@ from commandes.macros.stevendeo_macro import *
 from commandes.macros.tasneo_macro import *
 
 
+players = [
+"Agmar",
+"Idaho",
+"Jaenne",
+"Lex",
+"Mel",
+"PYo",
+"Stevendeo",
+"tasneo",
+]
+
+players_macro = [ #liste de fonctions
+macro_agmar_dm,
+macro_idaho_dm,
+macro_jaenne_dm,
+macro_lex_dm,
+macro_mel_dm,
+macro_pyo_dm,
+macro_stevendeo_dm,
+macro_tasneo_dm,
+]
+
+characters_macro = [ #liste de fonctions
+macro_agmar,
+macro_gargrim,
+macro_personnages_jaenne,
+macro_filtch,
+macro_willow,
+macro_eustache,
+macro_duncan,
+macro_darius,
+]
+
+
 def is_it_macro(message, client):
     canal = message.channel
     text = "not a macro"
 
-    if (message.author != client.user):
-
-        if (message.author.name == "Agmar"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_agmar_dm, macro_agmar)
-            
-        elif (message.author.name == "Idaho"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_idaho_dm, macro_gargrim)
-
-        elif (message.author.name == "Jaenne"): #"Jaenne"
-            canal, text = is_it_a_macro_for_nickname(message, macro_jaenne_dm, macro_personnages_jaenne)
-    
-        elif (message.author.name == "Lex"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_lex_dm, macro_filtch)
-            
-        elif (message.author.name == "Mel"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_mel_dm, macro_willow)
-            
-        elif (message.author.name == "PYo"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_pyo_dm, macro_eustache)
-            
-        elif (message.author.name == "Stevendeo"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_stevendeo_dm, macro_duncan)
-
-        elif (message.author.name == "tasneo"):
-            canal, text = is_it_a_macro_for_nickname(message, macro_tasneo_dm, macro_darius)
-          
+    for i in range(len(players)):
+        if message.author.name == players[i]:
+            canal, text = is_it_a_macro_for_nickname(message, players_macro[i], characters_macro[i])
     return canal, text
+
 
 def is_it_a_macro_for_nickname(message, macro_nickname_dm, macro_personnage):
     canal = message.channel
