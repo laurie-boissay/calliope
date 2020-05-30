2de67936d1ae1082cec03e38bbd73dbb9531af14
Jaenne <59830376+laurie-boissay@users.noreply.github.com>
2020-05-30 17:19:14 +0200

    Add files via upload

--- commandes/all_cmd.py
+++ commandes/all_cmd.py
@@ -16,6 +16,7 @@ from generation.generer_quete import *
 from generation.generer_commanditaire import *
 from generation.generer_nom import *
 from generation.generer_pj import *
+from collection_de_mots.personnes import *
 
 from collection_de_mots.calliope_reaction import *
 
@@ -49,11 +50,6 @@ def is_it_cmd(message, client):
         return canal, text
 
 def all_users_cmd(message):
-    plus_de_quetes = [
-    "!voler", "!infiltrer", "!protéger", "!livrer", "!enquêter",
-    "!kidnaper", "!tuer", "!détruire", "!trouver", "!sauver",
-    "!fabriquer", "!capturer", "!empoisonner", "!intercepter"
-    ]
 
     if message.startswith('!help'):
         text = [
@@ -63,6 +59,7 @@ def all_users_cmd(message):
         "\n\n```!+quetes```Affiche les commandes pour choisir le type de quête."
         "\n\n```!zone```Génère une zone."
         "\n\n```!pnj```Génère un personnage."
+        "\n\n```!personnage```Affiche les détais de la commande permettant de créer un PJ ou PNJ personnalisé."
         "\n\n```!auberge```Génère un nom d'auberge."
         "\n\n```!1d20-5   !1d20+5```Lance 1 dé 20 +/-5."
         "\n\n```!macro```Affiche les macros disponibles pour tes personnages."
@@ -97,7 +94,7 @@ def all_users_cmd(message):
         ]
         return text[0]
 
-    elif message in plus_de_quetes:
+    elif message.strip("!") in quete:
         text = afficher_quete(message.strip("!"))
         if len(text) < 1950: # max discord text lenght = 2000.
             return text
@@ -113,6 +110,30 @@ def all_users_cmd(message):
     elif message.startswith('!auberge'):
         return nom_auberge()
 
+    elif message.startswith('!races'):
+        text = "Vous pouvez choisir la race des :\n"
+        for i in range(len(pers_race)):
+            text += pers_race[i] + ", "
+        return text
+
+    elif message.startswith('!genres'):
+        text = ""
+        for i in range(len(pers_genre)):
+            text += pers_genre[i] + ", "
+        return text
+
+    elif message.startswith('!métiers'):
+        text = [
+        "```!pj, 12, 3, métier=archer/archère```\n"
+        "A noter que :"
+        "```!pj, 12, 3, métier=archer/archère de la garde royale```"
+        "Favorise aussi la caractéristique dextérité lors de la distribution des points de compétences.\n"
+        '''Seul le premier mot sans espace après le signe = compte. Ici c'est "archer/archère".\n\n'''
+        ]
+        for k, v in metiers_et_carac_associee.items():
+            text[0] += k + " : " + v + "\n"
+        return text[0]
+
     elif message.startswith('!macro'):
         return "Tu n'as pas encore de macro.\nDemande à Jaenne pour en ajouter."
 
@@ -126,6 +147,23 @@ def all_users_cmd(message):
 
     elif message.startswith('!pj'):
         return arguments_reroll(message)
+
+    elif message.startswith('!personnage'):
+        text = [
+        "\n\n```!pj, x, y```Génère un pj x et y sont obligatoires."
+        "\nx correspond au points de caractéristiques à dépenser."
+        "\ny correspond au maximum de points à dépenser dans une caractéristique."
+        "\n```!pj, 12, 3```Exemple de PJ généré sans aucun paramètre facultatif."
+
+        "\n\nTu peux préciser autant de paramètres facultatifs que tu le souhaites en les séparant chacun d'une virgule."
+        "\nJe comblerait tous les paramètres laissés vides :"
+        "\n```!pj, 12, 3, prénom=Toto, nom=du clan de Toto, race=orc, métier=mage, age=20, ville=Trifouillis les oies, genre=androgyne, secret=est violent```"
+
+        "\n\n```!races```Affiche la liste des races qui influencent le choix du nom et du prénom."
+        "\n\n```!genres```Affiche la liste des genres qui influencent le choix du nom et du prénom."
+        "\n```!métiers```Affiche la liste des métiers qui favorisent une caractéristiques."
+        ]
+        return text[0]
 
     else:
         return "not a cmd"
